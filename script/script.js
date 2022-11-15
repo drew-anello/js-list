887cb7676ba1965b0606fe34f88a89c9e4f2e213
Drew Anello <drew.anello@gmail.com>
2022-11-14 20:10:09 -0500

    adds elements

--- script/script.js
+++ script/script.js
@@ -3,7 +3,9 @@ const e = require('express')
 const formEl = document.getElementById('form')
 const textEl = document.getElementById('text')
 const numberEl = document.getElementById('number')
-
+const outputEl = document.getElementById('output')
+const number = parseInt(numberEl.value)
 formEl.addEventListener('submit', async e => {
   e.preventDefault()
 })
+const
