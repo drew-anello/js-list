583ae733651ca6647d37f87a27f282521f757310
Drew Anello <drew.anello@gmail.com>
2022-11-14 19:49:10 -0500

    adds elements and submit buttons

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const e = require('express')
+
+const formEl = document.getElementById('form')
+const textEl = document.getElementById('text')
+const numberEl = document.getElementById('number')
+
+formEl.addEventListener('submit', async e => {
+  e.preventDefault()
+})
