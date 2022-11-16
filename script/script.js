2faec286cacf01eb83612abd00a072e82514c619
Drew Anello <drew.anello@gmail.com>
2022-11-16 11:42:58 -0700

    wip

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
+const { text } = require('express')
+
 const formEl = document.getElementById('form')
 const textEl = document.getElementById('text')
 const numberEl = document.getElementById('number')
 const outputEl = document.getElementById('output')
 const number = parseInt(numberEl.value)
-const span = document.createElement('SPAN')
-const li = document.createElement('li')
 formEl.addEventListener('submit', async e => {
   e.preventDefault()
+  return
+  ;<li>textEl.append</li>
 })
-onclick === li.appendChild ''
